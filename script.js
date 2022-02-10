1d3e2a40bf0085029a2053e5a7401cb21c51a4c7
ziesorx <nwisessan@hotmail.com>
2022-02-11 02:03:14 +0700

    update random

--- script.js
+++ script.js
@@ -1,32 +1,80 @@
 let res;
 let ran;
-const arrRan = [
-  "ramen",
-  "bonchon",
-  "karayama",
-  "shinkansen",
-  "pepper Lunch",
-  "Food court",
-  "Swensen",
-];
-const dynaRan = [];
-// const newItem = String(document.querySelector(".newItem").value);
-// const newIt = String(document.getElementById("inputItem").value);
+const addbtn = document.querySelector('.newItem__btn');
+const removebtn = document.querySelector('.removeItem__btn');
+const inputNewItem = document.querySelector('.newItem__input');
+let result = document.querySelector('.result__input');
+const randombtn = document.querySelector('.button');
+const choice = document.querySelector('.choices__list');
 
-const randomEl = () => {
-  ran = Math.trunc(Math.random() * arrRan.length);
-  res = arrRan[ran];
-  console.log(res);
-  document.getElementById("result").textContent = res;
+// const choicesList = [
+//   'ramen',
+//   'bonchon',
+//   'karayama',
+//   'shinkansen',
+//   'pepper Lunch',
+//   'Food court',
+//   'Swensen',
+// ];
+
+const dynaChoices = [];
+
+const displayChoices = function (choicesList) {
+  choice.textContent = choicesList.join(', ');
+};
+// displayChoices(choicesList);
+
+const randomEl = choicesList => {
+  ran = Math.trunc(Math.random() * choicesList.length);
+  console.log(ran);
+  res = choicesList[ran];
+  result.textContent = res;
   return res;
 };
 
-// document.querySelector(".newItem").addEventListener("keydown", function (e) {
-//   if (e.key === "Enter") {
-//     console.log(newIt);
-//     dynaRan.push(this.newIt);
-//     console.log(dynaRan);
-//     return dynaRan;
-//   }
-// });
-document.querySelector(".button").addEventListener("click", randomEl);
+const capitalized = str => str[0].toUpperCase() + str.slice(1);
+
+// Add item
+const addItem = function (e) {
+  e.preventDefault();
+
+  if (inputNewItem.value !== '') {
+    dynaChoices.push(capitalized(inputNewItem.value));
+  }
+  inputNewItem.value = '';
+  displayChoices(dynaChoices);
+};
+
+// Remove item
+const removeItem = function (e) {
+  e.preventDefault();
+
+  if (
+    inputNewItem.value !== '' &&
+    capitalized(inputNewItem.value) === dynaChoices.some(choice => choice)
+  ) {
+    dynaChoices.splice(1, dynaChoices.indexOf(capitalized(inputNewItem.value)));
+  }
+  console.log(dynaChoices);
+  inputNewItem.value = '';
+  displayChoices(dynaChoices);
+};
+
+let i = 0;
+
+// Loop function
+const loopResult = function () {
+  setTimeout(() => {
+    // randomEl(choicesList);
+    randomEl(dynaChoices);
+    i++;
+    if (i < 20) {
+      loopResult();
+    }
+  }, 100);
+};
+
+addbtn.addEventListener('click', addItem);
+removebtn.addEventListener('click', removeItem);
+
+randombtn.addEventListener('click', loopResult);
