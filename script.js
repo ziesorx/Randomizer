60efae6a5ecc5ce2ccb14a484abe227100e0226e
ziesorx <nwisessan@hotmail.com>
2022-02-17 21:00:11 +0700

    update database

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let res;
 let ran;
-const addbtn = document.querySelector('.newItem__btn');
-const removebtn = document.querySelector('.removeItem__btn');
-const inputNewItem = document.querySelector('.newItem__input');
-let result = document.querySelector('.result__input');
-const randombtn = document.querySelector('.button');
-const choice = document.querySelector('.choices__list');
+const addbtn = document.querySelector(".newItem__btn");
+const removebtn = document.querySelector(".removeItem__btn");
+const inputNewItem = document.querySelector(".newItem__input");
+let result = document.querySelector(".result__input");
+const randombtn = document.querySelector(".button");
+const choice = document.querySelector(".choices__list");
+const midbut = document.querySelector(".mid-button");
+const rigged = ["pranakorn boat noodle"];
 
 // const choicesList = [
 //   'ramen',
@@ -18,13 +20,42 @@ const choice = document.querySelector('.choices__list');
 // ];
 
 const dynaChoices = [];
+const midTownHots = [
+  "mk",
+  "pepper lunch",
+  "tenya",
+  "A-ramen",
+  "pizza company",
+  "hachiban",
+  "food court",
+  "taco bell",
+  "nitaya chicken",
+  "bonchon",
+  "shinkansen",
+  "real beef",
+  "kfc",
+  "yayoi",
+  "spicy house",
+  "bbq plaza",
+  "sushi hiro",
+  "tum muo",
+  "karayama",
+  "pranakorn boat noodle",
+  "korean town",
+  "coco ichibanya",
+];
+
+const pushMid = () => {
+  midTownHots.forEach((i) => dynaChoices.push(i));
+  displayChoices(dynaChoices);
+};
 
 const displayChoices = function (choicesList) {
-  choice.textContent = choicesList.join(', ');
+  choice.textContent = choicesList.join(", ");
 };
 // displayChoices(choicesList);
 
-const randomEl = choicesList => {
+const randomEl = (choicesList) => {
   ran = Math.trunc(Math.random() * choicesList.length);
   // console.log(ran);
   res = choicesList[ran];
@@ -32,16 +63,16 @@ const randomEl = choicesList => {
   return res;
 };
 
-const capitalized = str => str[0].toUpperCase() + str.slice(1);
+const capitalized = (str) => str[0].toUpperCase() + str.slice(1);
 
 // Add item
 const addItem = function (e) {
   e.preventDefault();
 
-  if (inputNewItem.value !== '') {
+  if (inputNewItem.value !== "") {
     dynaChoices.push(capitalized(inputNewItem.value));
   }
-  inputNewItem.value = '';
+  inputNewItem.value = "";
   displayChoices(dynaChoices);
 };
 
@@ -51,30 +82,35 @@ const removeItem = function (e) {
   e.preventDefault();
 
   if (
-    inputNewItem.value !== '' &&
-    dynaChoices.some(choice => capitalized(inputNewItem.value) === choice)
+    inputNewItem.value !== "" &&
+    dynaChoices.some((choice) => capitalized(inputNewItem.value) === choice)
   ) {
     dynaChoices.splice(1, dynaChoices.indexOf(capitalized(inputNewItem.value)));
   }
-  inputNewItem.value = '';
+  inputNewItem.value = "";
   displayChoices(dynaChoices);
 };
 
 let i = 0;
-
 // Loop function
 const loopResult = function () {
   setTimeout(() => {
     // randomEl(choicesList);
     randomEl(dynaChoices);
     i++;
-    if (i < 30) {
+    if (i < 20) {
       loopResult();
     }
+    // else {
+    //   randomEl(rigged);
+    // }
   }, 100);
 };
 
-addbtn.addEventListener('click', addItem);
-removebtn.addEventListener('click', removeItem);
-
-randombtn.addEventListener('click', loopResult);
+midbut.addEventListener("click", pushMid);
+addbtn.addEventListener("click", addItem);
+removebtn.addEventListener("click", removeItem);
+randombtn.addEventListener("click", () => {
+  i = 0;
+  loopResult();
+});
